912d855ff0f2b2bdbc9cfa191efbdadb2174ded8
RedLeZ <109610210+RedLeZ@users.noreply.github.com>
2023-10-21 19:23:29 +0100

    Update something.py

--- something.py
+++ something.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+user_input = input("Enter a number: ")
+
+try:
+    # Convert the user input to an integer
+    user_input = int(user_input)
+
+    if user_input >= 0:
+        print("Valid input: It's a non-negative number.")
+    else:
+        print("Invalid input: It's a negative number.")
+except ValueError:
+    print("Invalid input: It's not a valid number.")
